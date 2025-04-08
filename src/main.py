c3406df784bd4c9aea47630ca8e99782164afb2c
northego1 <meleshuk0804@gmail.com>
2025-04-08 11:20:32 +0300

    added readme

--- src/main.py
+++ src/main.py
@@ -37,5 +37,5 @@ def create_app() -> FastAPI:
 app = create_app()
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", reload=True)
